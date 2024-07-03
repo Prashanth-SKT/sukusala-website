9860847c31dd84dfc9d58b9595e5e277d01f58b6
t.sai <t.sai@aligntech.com>
2024-07-03 23:40:42 +0530

    Website first commit - Fixed mail errors

--- contactus.php
+++ contactus.php
@@ -9,7 +9,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $subject = "New Contact Form Submission";
     $headers = "From: " . $email . "\r\n";
     $headers .= "Reply-To: " . $email . "\r\n";
-    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+    $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
 
     $body = "Contact Form Submission\n\n";
     $body .= "Name: " . $name . "\n";
--- mailquote.php
+++ mailquote.php
@@ -10,7 +10,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $subject = "New Quote Form Submission for . $category .";
     $headers = "From: " . $email . "\r\n";
     $headers .= "Reply-To: " . $email . "\r\n";
-    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+    $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
 
     $body = "Quote Form Submission\n\n";
     $body .= "Name: " . $name . "\n";
